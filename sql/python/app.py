fc5feee09203b020f65234d291eac24ca4696bb5
gorlapraveenkumar-netizen <gorlapraveenkumar6566@gmail.com>
2026-04-14 17:05:20 +0530

    Calculate total revenue from individual products

--- sql/python/app.py
+++ sql/python/app.py
@@ -1,3 +1,7 @@
-products=["phone","laptop","earphones","phone","laptop"]
-revenue=[20000,50000,4000,20000,100000]
-print("revenue generated:",sum(revenue))
+revenue1 = 20000 * 1
+revenue2 = 50000 * 1
+revenue3 = 2000 * 2
+revenue4 = 20000 * 1
+revenue5 = 50000 * 2
+total_revenue = revenue1 + revenue2 + revenue3 + revenue4 + revenue5
+print("Total Revenue:", total_revenue)
