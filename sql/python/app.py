77181f56d85910f3485889b289b7d2d99391db73
G Praveen Kumar <gorlapraveenkumar6566@gmail.com>
2026-04-18 15:50:35 +0530

    Delete sql/python directory

--- sql/python/app.py
+++
@@ -1,7 +0,0 @@
-revenue1 = 20000 * 1
-revenue2 = 50000 * 1
-revenue3 = 2000 * 2
-revenue4 = 20000 * 1
-revenue5 = 50000 * 2
-total_revenue = revenue1 + revenue2 + revenue3 + revenue4 + revenue5
-print("Total Revenue:", total_revenue)
